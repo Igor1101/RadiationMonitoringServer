5034efe273712b7dda894ff745539fe06e67aa73
Igor <igor.muravyov.2021.pz@ukr.net>
2021-07-06 11:46:26 +0100

    add support for freeing socket identifier

--- server.cpp
+++ server.cpp
@@ -20,20 +20,41 @@ server::server(QObject *parent) : QObject(parent)
 
 void server::init()
 { 
+    // open serv + listen 
+    int sockfd = 0;
+    struct sockaddr_in serv_addr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {
+        QINFO << U("ERROR cant open socket");
+        exit(EXIT_FAILURE);
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    
+    if(bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(sockaddr_in)) < 0) {
+        QDEB << U("cant bind socket");
+        exit(EXIT_FAILURE);
+    }
+                        
+    if(listen(sockfd, SOMAXCONN) < 0) {
+        QDEB << "err listen to sockfd";
+    }
+    qtcp_serv->setSocketDescriptor(sockfd);
     //fobj.setName(LOG_file);
     /*if(!fobj.open(QFile::WriteOnly| QFile::Text | QFile::Append)){
         QINFO <<"Failed to open "<<LOG_file;
         exit(1);
     }*/
 
-    status = qtcp_serv->listen(QHostAddress::Any, port);
+    /*status = qtcp_serv->listen(QHostAddress::Any, port);
     if(status) {
         QINFO << U("Сервер чекає на клієнтів, порт:") <<  port;
     } else {
         QCRI << U("Помилка запуска сервера, не може чекати клієнтів");
         QCRI << qtcp_serv->serverError() << qtcp_serv->errorString();
         exit(-1);
-    }
+    }*/
     // create or open DB
     db = new db_mng(db_file);
 }
@@ -47,10 +68,18 @@ void server::newConnection()
     if(setsockopt(socket->socketDescriptor(), SOL_SOCKET, SO_REUSEADDR, (const char*)&flag, sizeof(int)) < 0 ) { 
         QINFO << ("cant set SO_REUSEADDR");
     } 
+    if(setsockopt(socket->socketDescriptor(), IPPROTO_TCP, TCP_NODELAY, (const char*)&flag, sizeof(int)) < 0 ) { 
+        QINFO << ("cant set TCP_NODELAY");
+    } 
+    struct linger l = {1, 0};
+    if(setsockopt(socket->socketDescriptor(), SOL_SOCKET, SO_LINGER, (const char*)&l, sizeof(l)) < 0 ) { 
+        QINFO << ("cant set SOL_LINGER");
+    } 
+
     clients[idusersocs]=socket;
     // append signal from this user
     connect(clients[idusersocs],SIGNAL(readyRead()),this, SLOT(readClient()));
-    socket->setSocketOption(QAbstractSocket::KeepAliveOption,1);
+    //socket->setSocketOption(QAbstractSocket::KeepAliveOption,1);
 }
 
 void server::readClient()
@@ -95,6 +124,8 @@ void server::readClient()
 //    QJsonDocument jall = db->get_all();
     //QString res = QString(arr);
     QDEB << "recv:id:" + QString::number(idusersocs) ;//+ "data:" + QString(jall.toJson());
+    clientSocket->disconnect();
+    clientSocket->close();
 }
 
 QJsonDocument server::client_data_addtimestamp(QJsonDocument jdoc, qint64* timestamp)
